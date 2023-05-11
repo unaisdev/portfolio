f4bd5f56122ee8c92e2e2ad403695a0e2fe5095f
unaicanales <unaicanales@gmail.com>
2023-05-11 02:59:38 +0200

    icons problem solved

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import "./styles.css";
 import SocialMediaButtons from '../SocialMediaButtons';
 
