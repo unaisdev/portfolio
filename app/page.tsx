32ed050ab0a22393661cac5f7373e64d7564f709
unaicanales <unaicanales@gmail.com>
2023-05-11 03:20:28 +0200

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,11 @@ import SocialMediaButtons from '@/components/SocialMediaButtons'
 export default function Home() {
   return (
     <ThemeProvider attribute="class">
-      <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
-        <h1 className="text-5xl text-white font-bold mb-8 animate-bounce">
+      <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center px-10">
+        <h1 className="text-5xl text-white font-bold mb-8 animate-bounce text-center">
           Coming Soon
         </h1>
-        <p className="text-white text-lg mb-8">
+        <p className="text-white text-lg mb-8 text-center">
           Estoy trabajando en la web, te dejo mis redes aquí abajo
         </p>
         <div className='flex'>
