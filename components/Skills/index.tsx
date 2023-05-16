8dc457e567a5ee519caede550a8b293707347276
unaicanales <unaicanales@gmail.com>
2023-05-16 03:50:50 +0200

    ou

--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -36,7 +36,7 @@ const Skills = () => {
                 </div> */}
             </div>
 
-            <div className="flex flex-row flex-wrap justify-center divide-x-0 py-4 sm:py-12">
+            <div className="flex flex-row flex-wrap justify-center divide-x-0 py-6 sm:py-12">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" className="mx-3 h-12 w-12" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
