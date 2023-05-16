8ad30d743cee6cf3f3de1554ff4abbeafd968709
unaicanales <unaicanales@gmail.com>
2023-05-16 03:50:23 +0200

    responsiveness

--- components/LastProjects/index.tsx
+++ components/LastProjects/index.tsx
@@ -10,7 +10,7 @@ import ProjectList from './components/ProjectList';
 const LastProjects = () => {
 
     return (
-        <section className="py-12">
+        <section className="py-4 sm:py-12">
             <div className="flex justify-between flex-col md:flex-row">
                 <div className="flex flex-row px-6">
                     <svg xmlns="http://www.w3.org/2000/svg" className="mr-4" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -1,6 +1,6 @@
 const Skills = () => {
     return (
-        <section className="py-12">
+        <section className="py-4 sm:py-12">
             <div className="flex justify-between flex-col md:flex-row">
                 <div className="flex flex-row px-6">
                     <svg xmlns="http://www.w3.org/2000/svg" className="mr-4" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -36,7 +36,7 @@ const Skills = () => {
                 </div> */}
             </div>
 
-            <div className="flex flex-row flex-wrap justify-center divide-x-0">
+            <div className="flex flex-row flex-wrap justify-center divide-x-0 py-4 sm:py-12">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" className="mx-3 h-12 w-12" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
