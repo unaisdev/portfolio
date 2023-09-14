eec36350075787a830f1e149520545355cdaff21
unaicanales <unaicanales@gmail.com>
2023-09-14 17:11:33 +0200

    added host to config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'],
-    },
+  images: {
+    domains: ["images.unsplash.com", "i.ibb.co"],
+  },
+};
 
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
